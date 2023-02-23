a297c0a2fba224476296f2fece21c951b17e84c5
unknown <sachitsinghbais777@gmail.com>
2023-02-24 00:08:10 +0530

    day-3

---
+++ src/component/Filter.jsx
@@ -0,0 +1,134 @@
+import { useState } from 'react'
+
+export default function Filter() {
+  const DATA = [
+    {
+      id: 1,
+      title: 'Enjoy studying English',
+      tags: [
+        {
+          id: 'tag1',
+          title: 'English',
+          slug: 'english',
+        },
+        {
+          id: 'tag2',
+          title: 'For kids',
+          slug: 'kids',
+        },
+      ],
+    },
+    {
+      id: 2,
+      title: 'Parlons français',
+      tags: [
+        {
+          id: 'tag3',
+          title: 'French',
+          slug: 'french',
+        },
+        { id: 'tag2', title: 'Kids', slug: 'kids' },
+      ],
+    },
+    {
+      id: 3,
+      title: 'Intermediate English',
+      tags: [
+        {
+          id: 'tag1',
+          title: 'English',
+          slug: 'english',
+        },
+        {
+          id: 'tag4',
+          title: 'Adults',
+          slug: 'adults',
+        },
+      ],
+    },
+    {
+      id: 4,
+      title: 'How to study French',
+      tags: [
+        {
+          id: 'tag3',
+          title: 'French',
+          slug: 'french',
+        },
+        {
+          id: 'tag4',
+          title: 'Adults',
+          slug: 'adults',
+        },
+      ],
+    },
+  ]
+
+  const [filterTags, setFilterTags] = useState([])
+
+  const filteredDATA = DATA.filter((node) =>
+    filterTags.length > 0
+      ? filterTags.every((filterTag) =>
+          node.tags.map((tag) => tag.slug).includes(filterTag)
+        )
+      : DATA
+  )
+
+  const filterHandler = (event) => {
+    if (event.target.checked) {
+      setFilterTags([...filterTags, event.target.value])
+    } else {
+      setFilterTags(
+        filterTags.filter((filterTag) => filterTag !== event.target.value)
+      )
+    }
+  }
+
+  return (
+    <>
+      <div>
+        <label htmlFor="english">
+          <input
+            type="checkbox"
+            onChange={filterHandler}
+            value="english"
+            id="english"
+          />
+          <span>English</span>
+        </label>
+        <label htmlFor="french">
+          <input
+            type="checkbox"
+            onChange={filterHandler}
+            value="french"
+            id="french"
+          />
+          <span>French</span>
+        </label>
+        <label htmlFor="kids">
+          <input
+            type="checkbox"
+            onChange={filterHandler}
+            value="kids"
+            id="kids"
+          />
+          <span>Kids</span>
+        </label>
+        <label htmlFor="adults">
+          <input
+            type="checkbox"
+            onChange={filterHandler}
+            value="adults"
+            id="adults"
+          />
+          <span>Adults</span>
+        </label>
+      </div>
+      <ul>
+        {filteredDATA.map((node) => (
+          <li key={node.id}>{node.title}</li>
+        ))}
+      </ul>
+    </>
+  )
+}
--- src/pages/Men.jsx
+++ src/pages/Men.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React,{useEffect,useState}from 'react'
+import Filter from '../component/Filter'
+import axios from "axios"
+import { Grid,Heading,Image,SimpleGrid} from '@chakra-ui/react'
+import ProductCard from '../component/ProductCard'
 
 const Men = () => {
+  const [men,setMen]=useState([])
+  const fetchMEN=()=>{
+    return axios.get(`http://localhost:8080/men`).then((res)=>setMen(res.data))
+  }
+  useEffect(()=>{
+   fetchMEN()
+  },[])
   return (
-    <div>Men</div>
+  //   <Grid templateColumns='repeat(4, 1fr)' columns={{sm:2,md:3,lg:4,base:1}} gap={5}>
+   
+  // </Grid>
+
+
+<SimpleGrid
+        className="main_container"
+        w="85%"
+        m={'auto'}
+        mt={10}
+         columns={{sm:2,md:2,lg:4,base:1}} 
+         spacing={10}>
+           {men.map((item)=><ProductCard {...item}/>)}
+         </SimpleGrid>
   )
 }
 
